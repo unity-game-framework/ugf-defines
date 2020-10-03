1cb74068d963362db668378bd55afc7961deb18e
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-03 16:09:40 +0300

    Update DefinesSettings.cs

--- Packages/UGF.Defines/Editor/DefinesSettings.cs
+++ Packages/UGF.Defines/Editor/DefinesSettings.cs
@@ -24,6 +24,21 @@ namespace UGF.Defines.Editor
             return TryGet(name, out EnabledProperty<string> define) && define;
         }
 
+        public bool SetEnable(string name, bool value)
+        {
+            if (TryGetIndex(name, out int index))
+            {
+                EnabledProperty<string> define = m_defines[index];
+
+                define.Enabled = value;
+
+                m_defines[index] = define;
+                return true;
+            }
+
+            return false;
+        }
+
         public EnabledProperty<string> Get(string name)
         {
             return TryGet(name, out EnabledProperty<string> define) ? define : throw new ArgumentException($"Define not found by the specified name: '{name}'.");
@@ -31,17 +46,30 @@ namespace UGF.Defines.Editor
 
         public bool TryGet(string name, out EnabledProperty<string> define)
         {
+            if (TryGetIndex(name, out int index))
+            {
+                define = m_defines[index];
+                return true;
+            }
+
+            define = default;
+            return false;
+        }
+
+        public bool TryGetIndex(string name, out int index)
+        {
             for (int i = 0; i < m_defines.Count; i++)
             {
-                define = m_defines[i];
+                EnabledProperty<string> define = m_defines[i];
 
                 if (define.Value == name)
                 {
+                    index = i;
                     return true;
                 }
             }
 
-            define = default;
+            index = default;
             return false;
         }
     }
