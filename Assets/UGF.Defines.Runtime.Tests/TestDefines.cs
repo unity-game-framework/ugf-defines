97f8325d0b953e00709340431f08cd750b46496c
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2020-10-03 21:00:56 +0300

    Add TestDefines

---
+++ Assets/UGF.Defines.Runtime.Tests/TestDefines.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace UGF.Defines.Runtime.Tests
+{
+    public class TestDefines
+    {
+        [Test]
+        public void Test1()
+        {
+#if TEST1
+            Assert.Pass();
+#else
+            Assert.Fail();
+#endif
+        }
+
+        [Test]
+        public void Test2()
+        {
+#if TEST2
+            Assert.Pass();
+#else
+            Assert.Fail();
+#endif
+        }
+    }
+}
